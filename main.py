633c0f97c79743a315820bf618b12d13708e1a66
Willian Frantz <willianluigii@gmail.com>
2018-06-18 21:43:02 -0300

    Removed description

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ def show_menu():
     except:
         pass
 
-
-def switch(value: int):
-    if value == 1:
-        server_side()
-
-    elif value == 2:
-        client_side()
-
-    else:
-        # TODO: 01
-        InvalidCodeException()
-
 if __name__ == '__main__':
     try:
         server_side()
@@ -33,16 +21,3 @@ if __name__ == '__main__':
         client_side(ip, port)
     except:
         pass
-
-
-'''
-
-Não vai mais existir Client e nem Server pelo menos para o usuário final, vai ser transparente como se fosse 
-ambos em um produto. A classe Client e Server deverão ser chamados respectivamente quando o opt for escolhido,
-iniciando a conexão e enviando as mensagens.
-
-Toda vez que o produto for aberto, ele iniciará um Server para ficar ouvindo a determinada porta.
-Se por ventura o usuário final do produto escolher enviar mensagens para algum destino especifico, então a classe
-Client deverá ser instanciada para que criar um socket com o Server de destino. 
-
-'''
